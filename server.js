948e3cb02ae8d0d4b46763bc18885ff7b8693d24
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2017-03-03 10:14:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,14 +21,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function(hash, salt) {
+function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, sha512);
     //return hashed.toString('hex');
     return['pbkdf2', '10000', salt, hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input', function(req,res){
    // var hashedString = hash(req.params.input, salt);  give some temporary salt value in next line
-    var hashedString = hash(req.params.input, this-is-some-random-string);
+   var hashedString = hash(req.params.input, this-is-some-random-string);
    res.send(hashedString);
     
 })
@@ -37,7 +37,7 @@ app.get('/hash/:input', function(req,res){
 var pool = new Pool(config);
 //app.get('/create-user', function (req, res) { // this is converted to a post request (see comments @ 14 to 15). . .
 // . . assuming that we will get the username and password from the request body
-   //this fn will take the  username & password and create an entry in the user table
+//this fn will take the  username & password and create an entry in the user table
    
   app.post('/create-user', function (req, res) {
   // Q: how to make a post request . . see @ 16.10 . .
@@ -57,7 +57,7 @@ var pool = new Pool(config);
      else{
          res.send('User successfully created: ' +username);
      }
-   })
+   });
    
 });
 // pool connection - to fill

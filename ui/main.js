e013e6402c2914427e81095a338b9ff631198237
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2017-03-06 13:41:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,30 @@
-console.log('Loaded!');
+//submit username, password to login
+var submit = document.getElementById("submit_btn");
+submit.onclick = function() {
+    
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in our variable
+    request.onreadystatechange = function () {
+       if (request.readyState === XMLHttpRequest.DONE) {
+         //TAKE SOME ACTION
+        if (request.status ===200) {
+        console.log("User logged in");
+        alert("Logged in successfully");
+       } else if (request.status===403) {
+           alert("Username/password is incorrect");
+        } else if (request.status===500) {
+            alert("Something went wrong on the server!");
+        }
+       }
+    };
+   // Make the request 
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  console.log("username");
+  console.log("password");
+  request.open('POST', 'http://sgovindan53.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify<{username: username, password: password}>);
+                
+};
